39aa296f2f2109b71e1ce6f4b645966215a612e8
J, Prakash <prakash.j@legato.com>
2023-09-07 18:23:48 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading  = React.createElement("h1",{id:"heading"},"Hello World");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-console.log("name")
+console.log("name");
